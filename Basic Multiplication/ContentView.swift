8d71984591d9a5a8b3c0ed71526cbff1d9daad13
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-20 08:46:37 +0200

    ADD basic main UI

--- Basic Multiplication/ContentView.swift
+++ Basic Multiplication/ContentView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 32) {
+            HStack() {
+                Text("1")
+                Text("x")
+                Text("1")
+            }
+            HStack(spacing: 16) {
+                Button("1") {
+
+                }
+                .frame(minWidth: 56, minHeight: 42)
+                .border(.accent)
+                Button("9") {
+
+                }
+                .frame(minWidth: 56, minHeight: 42)
+                .border(.accent)
+                Button("6") {
+
+                }
+                .frame(minWidth: 56, minHeight: 42)
+                .border(.accent)
+            }
+            Spacer()
+            Button("Next") {
+
+            }
+            .frame(maxWidth: .infinity, maxHeight: 64)
+            .border(.accent)
         }
         .padding()
     }
