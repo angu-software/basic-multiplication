ff5526e835c87ee9c326de57bcc6cb67a337afe1
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-20 09:35:21 +0200

    ADD SnapshotTests target

---
+++ SnapshotTests/MainUITests.swift
@@ -0,0 +1,21 @@
+//
+//  SnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by Andreas Guenther on 20.09.24.
+//
+
+import Testing
+
+import SnapshotTesting
+
+@testable import Basic_Multiplication
+
+@MainActor
+struct MainUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
