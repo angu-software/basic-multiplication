29612fbdcb95b947b221a76a66f4f9ac62aa187b
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-21 11:53:06 +0200

    MOD outline desired structure for SwiftSpec

--- Packages/SwiftSpec/Tests/SwiftSpecClient/ClientTests.swift
+++ Packages/SwiftSpec/Tests/SwiftSpecClient/ClientTests.swift
@@ -1,23 +1,49 @@
+import Testing
 import SwiftSpec
 
-import Testing
+enum Spec {
+
+    // #define
+    @Suite("NumberGenerator")
+    struct NumberGeneratorTests {
+
+        // #context
+        @Suite(".generateNumber")
+        struct GenerateNumberContext {
+
+            // #when
+            @Suite("when called")
+            struct WhenCalled {
+
+                // #it
+                @Test("it should generate a number between 0 and 10")
+                func it_should_generate_a_number_between_0_and_10() async throws {
+                    #expect(false == true)
+                }
+
+                // #it
+                @Test("it should generate a whole number")
+                func it_should_generate_a_whole_number_between_0_and_10() async throws {
+                    #expect(false == true)
+                }
+            }
+        }
+    }
+}
 
 //enum Spec {
 //
-//    #define("Foo") {
-//        #context("Bar") {
-//            #when("called") {
-//                #it("does something") {
+//    #define("NumberGenerator") {
+//        #context(".generateNumber") {
+//            #when("when called") {
+//                #it("it should generate a number between 0 and 10") {
+//                    #expect(false == true)
+//                }
+//
+//                #it("it should generate a whole number") {
 //                    #expect(false == true)
 //                }
 //            }
 //        }
 //    }
 //}
-
-struct Tests {
-    @Test("Hello")
-    func hello() async throws {
-        #expect(true == false)
-    }
-}
