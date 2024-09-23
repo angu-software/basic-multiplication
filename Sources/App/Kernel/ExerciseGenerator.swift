af018397ebaeb8c0b6a1ef8a20d12aacd4254c59
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-23 14:44:13 +0200

    ADD ExerciseGenerator.Configuration

--- Sources/App/Kernel/ExerciseGenerator.swift
+++ Sources/App/Kernel/ExerciseGenerator.swift
@@ -8,6 +8,31 @@
 
 struct ExerciseGenerator {
 
+    struct Configuration {
+
+        static func numberRangeUpTo100() -> Self {
+            Self(operandRange: 0...10,
+                 numberOfSuggestions: 3,
+                 maxSuggestionRange: 0...100,
+                 maxSuggestionDistanceToProduct: 3)
+        }
+
+        let operandRange: ClosedRange<Int>
+        let numberOfSuggestions: Int
+        let maxSuggestionRange: ClosedRange<Int>
+        let maxSuggestionDistanceToProduct: Int
+
+        init(operandRange: ClosedRange<Int>,
+             numberOfSuggestions: Int,
+             maxSuggestionRange: ClosedRange<Int>,
+             maxSuggestionDistanceToProduct: Int) {
+            self.operandRange = operandRange
+            self.numberOfSuggestions = numberOfSuggestions
+            self.maxSuggestionRange = maxSuggestionRange
+            self.maxSuggestionDistanceToProduct = maxSuggestionDistanceToProduct
+        }
+    }
+
     static let operandRange = 0...10
     static let numberOfSuggestions = 3
     static let maxSuggestionRange = 0...100
