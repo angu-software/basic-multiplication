a7628fd5deaa076770188bc797ff838dbbe27709
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-24 17:10:28 +0200

    ADD Texts definitions for the MultiplicationExerciseView

---
+++ Sources/App/MainUI/Screens/MultiplicationExerciseView/Texts.swift
@@ -0,0 +1,15 @@
+//
+//  Texts.swift
+//  Basic Multiplication
+//
+//  Created by Andreas Guenther on 24.09.24.
+//
+
+extension MultiplicationExerciseView {
+
+    enum Texts {
+
+        static let continueButtonTitle = "Next exercise"
+        static let operationFormat = "%d x %d"
+    }
+}
