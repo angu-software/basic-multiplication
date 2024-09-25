c94d6e7c892e73efc6e241f102185d71f9f46d7a
Andreas Günther <andreas.guenther@runtastic.com>
2024-09-25 11:41:57 +0200

    MOD convert SnapshotTests to XCTest to have failure diffs attached to the test. Not supported in SwiftTesting

--- Tests/SnapshotTests/MultiplicationExerciseViewTests.swift
+++ Tests/SnapshotTests/MultiplicationExerciseViewTests.swift
@@ -5,21 +5,21 @@
 //  Created by Andreas Guenther on 20.09.24.
 //
 
-import Testing
+import XCTest
 
 import SnapshotTesting
 import SwiftUI
 
 @testable import Basic_Multiplication
 
-struct MultiplicationExerciseViewTests {
+final class MultiplicationExerciseViewTests: XCTestCase {
 
     @MainActor
-    @Suite("When initially shown")
-    struct WhenInitiallyShown {
+    // "When initially shown"
+    final class WhenInitiallyShown: XCTestCase {
 
-        @Test("it should vertically layout operation and suggestions")
-        func it_should_vertically_layout_operation_and_suggestions() async throws {
+        // it should vertically layout operation and suggestions
+        func test_it_should_vertically_layout_operation_and_suggestions() async throws {
             let view = MultiplicationExerciseView(viewState: ViewState(operation: "1 x 1",
                                                                        productSuggestions: ["1", "9", "6"]))
 
@@ -30,10 +30,10 @@ struct MultiplicationExerciseViewTests {
     }
 
     @MainActor
-    @Suite("When the correct suggestion is selected")
-    struct WhenTheCorrectSuggestionIsSelected {
-        @Test("it indicates the selection is correct")
-        func it_indicates_the_selection_is_correct() async throws {
+    // "When the correct suggestion is selected"
+    final class WhenTheCorrectSuggestionIsSelected: XCTestCase {
+        // "it indicates the selection is correct"
+        func test_it_indicates_the_selection_is_correct() async throws {
             let state = ViewState(operation: "1 x 1",
                                   productSuggestions: ["1", "9", "6"],
                                   isContinueButtonDisabled: false,
@@ -49,10 +49,10 @@ struct MultiplicationExerciseViewTests {
     }
 
     @MainActor
-    @Suite("When the wrong suggestion is selected")
-    struct WhenTheWrongSuggestionIsSelected {
-        @Test("it indicates the selection is wrong")
-        func it_indicates_the_selection_is_wrong() async throws {
+    // "When the wrong suggestion is selected"
+    final class WhenTheWrongSuggestionIsSelected: XCTestCase {
+        // "it indicates the selection is wrong"
+        func test_it_indicates_the_selection_is_wrong() async throws {
             let state = ViewState(operation: "1 x 1",
                                   productSuggestions: ["1", "9", "6"],
                                   isContinueButtonDisabled: false,
