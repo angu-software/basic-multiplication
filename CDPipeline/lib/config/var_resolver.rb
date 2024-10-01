00bc265390ce1caad720e33abdadc3b94d9e4630
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-01 09:43:02 +0200

    MOD extract method to make a var from a name

--- CDPipeline/lib/config/var_resolver.rb
+++ CDPipeline/lib/config/var_resolver.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# TODO: support Parameter/Variable Expansion format: ${VAR}
+
 class VarResolver
   attr_reader :environment
 
@@ -9,13 +11,18 @@ class VarResolver
 
   def resolve(str)
     env_keys = environment.keys
-    return str unless env_keys.any? { |key| str.include?("$#{key}") }
+    return str unless env_keys.any? { |key| str.include?(make_var(key)) }
 
     environment.each do |key, value|
-      # str = str.gsub("${#{key}}", value)
-      str = str.gsub("$#{key}", value)
+      str = str.gsub(make_var(key), value)
     end
 
     resolve(str)
+  end
+
+  private
+
+  def make_var(name)
+    "$#{name}"
   end
 end
