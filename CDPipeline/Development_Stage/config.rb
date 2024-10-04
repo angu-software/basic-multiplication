39aba033a81d4a5c32245b2c0b1785008c88f5de
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-04 14:25:19 +0200

    MOD use short commit SHA and better names build folder

--- CDPipeline/Development_Stage/config.rb
+++ CDPipeline/Development_Stage/config.rb
@@ -2,7 +2,7 @@
 
 require_relative 'git_info'
 
-BUILD_ID = GitInfo.git_commit_sha
+BUILD_ID = GitInfo.git_commit_sha(short: true)
 
 XCODE_PATH = '/Applications/Xcode_16.app'
 
@@ -13,9 +13,11 @@ BUILD_DESTINATION = 'generic/platform=iOS Simulator'
 TEST_DESTINATION = 'platform=iOS Simulator,name=iPhone 16 Pro'
 TEST_CODE_SIGNING_SETTINGS = "CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY='' CODE_SIGNING_ALLOWED=NO"
 
-ARTIFACT_NAME = "#{SCHEME}_#{BUILD_ID}".freeze
-BUILD_PATH = '.builds'
+ARTIFACT_NAME = "Basic_Multiplication_#{BUILD_ID}".freeze
+BUILD_PATH = '__builds__'
 DERIVED_DATA = "#{BUILD_PATH}/Derived_Data/#{ARTIFACT_NAME}".freeze
 PRODUCTS_PATH = "#{BUILD_PATH}/Products/#{ARTIFACT_NAME}".freeze
 ARTIFACTS_PATH = "#{PRODUCTS_PATH}/Test_build.xctestproducts".freeze
 TEST_RESULTS_PATH = "#{PRODUCTS_PATH}/Test_results.xcresult".freeze
+
+# TODO: secify error result bundle path
