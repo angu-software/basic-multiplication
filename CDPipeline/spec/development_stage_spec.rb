cec35ebe099712f1f89c1b11c1e358e4e3c5afca
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-04 08:06:26 +0200

    MOD ensure development_stage calls the necessary commands

--- CDPipeline/spec/development_stage_spec.rb
+++ CDPipeline/spec/development_stage_spec.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require_relative '../Development_Stage/config'
 require_relative '../Development_Stage/development_stage'
 
 describe DevelopmentStage do
+  let(:command_runner) { CommandRunner }
+  let(:git_info) { GitInfo }
+
   describe '.run' do
     subject { described_class.run }
 
@@ -11,16 +15,39 @@ describe DevelopmentStage do
       allow(described_class).to receive(:build_for_testing).and_call_original
       allow(described_class).to receive(:test_without_building).and_call_original
       allow(described_class).to receive(:tag_rc_build).and_call_original
-      allow(described_class).to receive(:run_command)
+
+      allow(git_info).to receive(:next_rc_build_tag).and_return('Staged-RC-3')
+      allow(git_info).to receive(:git_commit_sha).and_return('ab123')
+
+      allow(command_runner).to receive(:run)
+
+      subject
     end
 
-    it 'it exectues all steps' do
-      subject
-
+    it 'it exectues all steps sequentially' do
       expect(described_class).to have_received(:select_xcode).ordered
       expect(described_class).to have_received(:build_for_testing).ordered
       expect(described_class).to have_received(:test_without_building).ordered
       expect(described_class).to have_received(:tag_rc_build).ordered
+    end
+
+    it 'selects an xcode version' do
+      expect(command_runner).to have_received(:run).with("sudo xcode-select -s #{XCODE_PATH}")
+    end
+
+    it 'builds for testing' do
+      expect(command_runner).to have_received(:run).with(BUILD_FOR_TESTING_COMMAND)
+    end
+
+    it 'tests without building' do
+      expect(command_runner).to have_received(:run).with(TEST_WITHOUT_BUILDING_COMMAND)
+    end
+
+    context 'when tagging the rc build' do
+      it 'it runs the necessary git commands' do
+        expect(command_runner).to have_received(:run).with('git tag -a Staged-RC-3 ab123').ordered
+        expect(command_runner).to have_received(:run).with('git push origin Staged-RC-3').ordered
+      end
     end
   end
 end
