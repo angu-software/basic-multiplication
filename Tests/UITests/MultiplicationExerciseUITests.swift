5aba41f067bfe8b1580b561b537fbdc74e2562d6
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-11 11:58:12 +0200

    Remove the WhenSelectingASuggestion class which was meant to group the tests into a certain context. But this is not really working well with XCTests

--- Tests/UITests/MultiplicationExerciseUITests.swift
+++ Tests/UITests/MultiplicationExerciseUITests.swift
@@ -9,64 +9,62 @@ import XCTest
 
 /* Acceptance tests */
 
-final class MultiplicationExerciseUITests {
+@MainActor
+final class MultiplicationExerciseUITests: XCTestCase {
 
     // When selecting a suggestion
-    @MainActor
-    final class WhenSelectingASuggestion: XCTestCase {
 
-        private lazy var robot = MultiplicationExerciseRobot()
-        private var exercise: MultiplicationExerciseDSL!
+    private lazy var robot = MultiplicationExerciseRobot()
+    private var exercise: MultiplicationExerciseDSL!
 
-        override func setUpWithError() throws {
-            try super.setUpWithError()
+    override func setUpWithError() throws {
+        try super.setUpWithError()
 
-            continueAfterFailure = false
+        continueAfterFailure = false
 
-            exercise = MultiplicationExerciseDSL(protocolDriver: robot)
-        }
+        exercise = MultiplicationExerciseDSL(protocolDriver: robot)
+    }
 
-        override func tearDownWithError() throws {
-            robot.terminateApp()
+    override func tearDownWithError() throws {
+        robot.terminateApp()
 
-            try super.tearDownWithError()
-        }
+        try super.tearDownWithError()
+    }
 
-        /* Given: A random multiplication exercise with offered solutions
-         * When: The user selects the correct solution
-         * Then: It shows that the selection was correct
-         */
-        func test_it_shows_that_the_correct_suggestion_was_selected() throws {
-            try exercise.showsRandomExerciseWithOfferedSolutions()
+    /* Given: A random multiplication exercise with offered solutions
+     * When: The user selects the correct solution
+     * Then: It shows that the selection was correct
+     */
+    func test_it_shows_that_the_correct_suggestion_was_selected() throws {
+        try exercise.showsRandomExerciseWithOfferedSolutions()
 
-            try exercise.selectSolution(.correct)
+        try exercise.selectSolution(.correct)
 
-            exercise.assertSelectionFeedback(.correct)
-        }
+        exercise.assertSelectionFeedback(.correct)
+    }
 
-        /* Given: A random multiplication exercise with offered solutions
-         * When: The user selects the wrong solution
-         * Then: It shows that the selection was wrong
-         */
-        func test_it_shows_that_the_wrong_suggestion_was_selected() throws {
-            try exercise.showsRandomExerciseWithOfferedSolutions()
+    /* Given: A random multiplication exercise with offered solutions
+     * When: The user selects the wrong solution
+     * Then: It shows that the selection was wrong
+     */
+    func test_it_shows_that_the_wrong_suggestion_was_selected() throws {
+        try exercise.showsRandomExerciseWithOfferedSolutions()
 
-            try exercise.selectSolution(.wrong)
+        try exercise.selectSolution(.wrong)
 
-            exercise.assertSelectionFeedback(.wrong)
-        }
+        exercise.assertSelectionFeedback(.wrong)
+    }
 
-        /* Given: A random multiplication exercise with offered solutions
-         * When: The user selects a solution and proceeds to the next exercise
-         * Then: A new exercise is presented to the user
-         */
-        func test_it_generates_a_new_exercise_when_taping_continue() throws {
-            try exercise.showsRandomExerciseWithOfferedSolutions()
+    /* Given: A random multiplication exercise with offered solutions
+     * When: The user proceeds to the next exercise
+     * Then: A new exercise is presented to the user
+     */
+    func test_it_generates_a_new_exercise_when_tapping_continue() throws {
+        try exercise.showsRandomExerciseWithOfferedSolutions()
 
-            try exercise.selectSolution()
-            try exercise.proceedToNextExercise()
+        try exercise.selectSolution()
+        try exercise.proceedToNextExercise()
 
-            exercise.assertNewExerciseIsShown()
-        }
+        exercise.assertNewExerciseIsShown()
     }
 }
