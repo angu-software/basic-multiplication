5ac598406c3814348e7fbb6e7d612c55a9cb7040
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-15 14:33:21 +0200

    move all vars to .env

--- CDPipeline/dev_stage/run_dev_tests.sh
+++ CDPipeline/dev_stage/run_dev_tests.sh
@@ -3,20 +3,6 @@ set -e
 
 source ./CDPipeline/dev_stage/.env
 
-export BUILD_RESULT='BasicMultiplication.xcresult'
-export TEST_RESULT='DevelopmentTests.xcresult'
-
-# setting xcode version
-export DEVELOPER_DIR='/Applications/Xcode_16.app/Contents/Developer'
-
-export XCODEBUILD_SCHEME="$APP_SCHEME"
-export XCODEBUILD_TEST_PLAN='DevelopmentTests'
-export XCODEBUILD_DESTINATION='platform=iOS Simulator,name=iPhone 16 Pro,OS=18.0'
-export XCODEBUILD_DERIVED_DATA_PATH="$BUILD_DIR/derived_data"
-export XCODEBUILD_TEST_PRODUCTS_PATH="$BUILD_ARTIFACTS_DIR/$XCODEBUILD_TEST_PLAN.xctestproducts"
-export XCODEBUILD_BUILD_RESULT_BUNDLE_PATH="$BUILD_ARTIFACTS_DIR/$BUILD_RESULT"
-export XCODEBUILD_TEST_RESULT_BUNDLE_PATH="$BUILD_ARTIFACTS_DIR/$TEST_RESULT"
-
 xcodebuild \
 build-for-testing \
 -scheme "$XCODEBUILD_SCHEME" \
