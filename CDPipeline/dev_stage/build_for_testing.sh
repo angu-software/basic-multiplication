89d881720b007b8310f2ac3de566039bc360179a
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-15 14:36:47 +0200

    split build for testing and test witout building tasks into separate scripts

---
+++ CDPipeline/dev_stage/build_for_testing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+source ./CDPipeline/dev_stage/.env
+
+xcodebuild \
+build-for-testing \
+-scheme "$XCODEBUILD_SCHEME" \
+-destination "$XCODEBUILD_DESTINATION" \
+-derivedDataPath "$XCODEBUILD_DERIVED_DATA_PATH" \
+-testProductsPath "$XCODEBUILD_TEST_PRODUCTS_PATH" \
+-resultBundlePath "$XCODEBUILD_BUILD_RESULT_BUNDLE_PATH" \
+-skipUnavailableActions \
+CODE_SIGNING_REQUIRED=NO \
+CODE_SIGN_IDENTITY='' \
+CODE_SIGNING_ALLOWED=NO
