005b59f5f9b1b20f66abd1a16a7a57d6d01c5656
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-15 12:47:59 +0200

    Extract env vars fin run dev tests script

--- CDPipeline/dev_stage/run_dev_tests.sh
+++ CDPipeline/dev_stage/run_dev_tests.sh
@@ -3,18 +3,22 @@
 
       # Task: Execute Dev Tests
       export DEVELOPER_DIR='/Applications/Xcode_16.app/Contents/Developer'
+      export XCODEBUILD_SCHEME='Basic Multiplication'
+      export XCODEBUILD_TEST_PLAN='DevelopmentTests'
+      export XCODEBUILD_DESTINATION='platform=iOS Simulator,name=iPhone 16 Pro,OS=18.0'
+      export XCODEBUILD_DERIVED_DATA_PATH='.xcpipeline/derived_data'
+      export XCODEBUILD_TEST_PRODUCTS_PATH='.xcpipeline/artifacts/BasicMultiplication.xctestproducts'
+      export XCODEBUILD_RESULT_BUNDLE_PATH='.xcpipeline/artifacts/BasicMultiplication.xcresult'
 
       xcodebuild \
       test \
-      -scheme 'Basic Multiplication' \
-      -testPlan 'DevelopmentTests' \
-      -destination 'platform=iOS Simulator,name=iPhone 16 Pro,OS=18.0' \
-      -derivedDataPath ".xcpipeline/derived_data" \
-      -testProductsPath ".xcpipeline/artifacts/BasicMultiplication.xctestproducts" \
-      -resultBundlePath ".xcpipeline/artifacts/BasicMultiplication.xcresult" \
+      -scheme "$XCODEBUILD_SCHEME" \
+      -testPlan "$XCODEBUILD_TEST_PLAN" \
+      -destination "$XCODEBUILD_DESTINATION" \
+      -derivedDataPath "$XCODEBUILD_DERIVED_DATA_PATH" \
+      -testProductsPath "$XCODEBUILD_TEST_PRODUCTS_PATH" \
+      -resultBundlePath "$XCODEBUILD_RESULT_BUNDLE_PATH" \
       -skipUnavailableActions \
       CODE_SIGNING_REQUIRED=NO \
       CODE_SIGN_IDENTITY='' \
       CODE_SIGNING_ALLOWED=NO
-
-      unset DEVELOPER_DIR
