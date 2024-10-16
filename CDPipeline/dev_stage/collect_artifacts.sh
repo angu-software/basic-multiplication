7fcf8013d495666d3829a2458acbb2b01e7d586f
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-16 09:35:56 +0200

    [CD] return the path of the ziped artifact arcive

--- CDPipeline/dev_stage/collect_artifacts.sh
+++ CDPipeline/dev_stage/collect_artifacts.sh
@@ -3,4 +3,12 @@ set -e
 
 source $1
 
+# TODO: get the current build tag (build number)
+RC_TAG_PREFIX="Staged-RC-"
+UNIX_TIMESTAMP=$(date +%s)
+GIT_SHA=$(git rev-parse --short HEAD)
+ARTIFACT_ARCHIVE="$BUILD_DIR/${RC_TAG_PREFIX}${GIT_SHA}-${UNIX_TIMESTAMP}.zip"
+
 zip -qr "$ARTIFACT_ARCHIVE" "$BUILD_ARTIFACTS_DIR"
+
+echo "$ARTIFACT_ARCHIVE"
--- CDPipeline/dev_stage/tag_staged_rc.sh
+++ CDPipeline/dev_stage/tag_staged_rc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-source $1
-
 git fetch --tags
 ruby ./CDPipeline/Development_Stage/run.rb --tag_rc
