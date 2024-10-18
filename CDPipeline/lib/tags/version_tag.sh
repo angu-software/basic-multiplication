48f0a910858b79b0882f4d30deaabc80513e30ea
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-18 22:23:36 +0200

    [CD] Move split_tag into version_tag.sh

--- CDPipeline/lib/tags/version_tag.sh
+++ CDPipeline/lib/tags/version_tag.sh
@@ -1,5 +1,5 @@
 local SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-source $SCRIPT_DIR/split_tag.sh
+source $SCRIPT_DIR/../foundation/split.sh
 
 local INITIAL_VERSION="1.0.0"
 local INITIAL_BUILD_NUMBER=1
@@ -22,6 +22,35 @@ make_version_tag() {
 
 initial_version_rc_tag() {
     echo $(make_version_tag $INITIAL_VERSION $INITIAL_BUILD_NUMBER true)
+}
+
+# Splits a tag string into its components.
+# The tag string is expected to be in the format <major>.<minor>.<patch>+<build>[-RC].
+# Parameters: 
+#   $1 - The tag string to be split.
+# Returns:
+#   The components of the tag string.
+split_tag() {
+    local TAG=$1
+
+    if [[ -z "$TAG" ]]; then
+        echo ''
+        return
+    fi
+
+    local TAG_SPLIT=($(split $TAG '+'))
+    local VERSION=${TAG_SPLIT[0]}
+    local META=${TAG_SPLIT[1]}
+    local META_SPLIT=($(split $META '-'))
+    local BUILD_NUMBER=${META_SPLIT[0]}
+    local RC=${META_SPLIT[1]}
+
+    if [[ -z "$RC" ]]; then
+        echo "$VERSION $BUILD_NUMBER"
+        return
+    fi
+
+    echo "$VERSION $BUILD_NUMBER $RC"
 }
 
 build_number_from_tag() {
--- CDPipeline/spec/lib/tags/verstion_tag_spec.sh
+++ CDPipeline/spec/lib/tags/verstion_tag_spec.sh
@@ -3,6 +3,30 @@ Describe 'verstion_tag'
     local VERSION='1.2.3'
     local TAG="$VERSION+2-RC"
 
+    Describe 'split_tag()'
+        It 'it splits the tag into its compnents'
+            When call split_tag $TAG
+            The output should equal '1.2.3 2 RC'
+        End
+
+        Describe 'when tag dose not contain RC indicator'
+            TAG="$VERSION+3"
+
+            It 'it splits the tag into its compnents'
+                When call split_tag $TAG
+                The output should equal '1.2.3 3'
+            End
+        End
+
+        Describe 'when tag is empty'
+            TAG=''
+            It 'it returns an empty string'
+                When call split_tag $TAG
+                The output should equal ''
+            End
+        End
+    End
+
     Describe 'build_number_from_tag()'
         Describe 'when tag is empty'
             TAG=''
