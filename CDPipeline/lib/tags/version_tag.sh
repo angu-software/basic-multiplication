271a37e63dfb9d4d55803e5a9dd0d1d7f5335156
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-18 22:17:38 +0200

    [CD] Move next_rc_tag_from_tag into version_tag.sh

--- CDPipeline/lib/tags/version_tag.sh
+++ CDPipeline/lib/tags/version_tag.sh
@@ -28,7 +28,7 @@ build_number_from_tag() {
     local TAG=$1
 
     if [[ -z "$TAG" ]]; then
-        echo 1
+        echo $INITIAL_BUILD_NUMBER
         return
     fi
 
@@ -36,4 +36,18 @@ build_number_from_tag() {
     local BUILD_NUMBER=${SPLIT_TAG[1]}
 
     echo $BUILD_NUMBER
+}
+
+next_rc_tag_from_tag() {
+    local TAG=$1
+
+    if [[ -z "$TAG" ]]; then
+        echo "$(initial_version_rc_tag)"
+        return
+    fi
+
+    local CURRENT_BUILD_NUMBER=$(build_number_from_tag $TAG)
+    local NEXT_BUILD_NUMBER=$((CURRENT_BUILD_NUMBER + 1))
+
+    echo $(make_version_tag "1.0.0" $NEXT_BUILD_NUMBER true)
 }
--- CDPipeline/spec/lib/tags/verstion_tag_spec.sh
+++ CDPipeline/spec/lib/tags/verstion_tag_spec.sh
@@ -1,20 +1,41 @@
-Describe 'build_number_from_tag()'
+Describe 'verstion_tag'
     Include 'lib/tags/version_tag.sh'
+    local VERSION='1.2.3'
+    local TAG="$VERSION+2-RC"
 
-    TAG='1.0.0+2-RC'
+    Describe 'build_number_from_tag()'
+        Describe 'when tag is empty'
+            TAG=''
+            It 'it returns 1'
+                When call build_number_from_tag ''
+                The output should equal 1
+            End
+        End
 
-    Describe 'when tag is empty'
-        TAG=''
-        It 'it returns 1'
-            When call build_number_from_tag ''
-            The output should equal 1
+        Describe 'when tag is not empty'
+            It 'it returns the build number'
+                When call build_number_from_tag $TAG
+                The output should equal 2
+            End
         End
     End
 
-    Describe 'when tag is not empty'
-        It 'it returns the build number'
-            When call build_number_from_tag $TAG
-            The output should equal 2
+    Describe 'next_rc_tag_from_tag()'
+        Describe 'when tag is empty'
+            TAG=''
+
+            It 'it returns intial RC tag'
+                When call next_rc_tag_from_tag $TAG
+                The output should equal '1.0.0+1-RC'
+            End
+        End
+
+        Describe 'when tag exists'
+            Pending 'Needs extraction of version from tag'
+            It 'it returns the next RC tag'
+                When call next_rc_tag_from_tag $TAG
+                The output should equal "$VERSION+2-RC"
+            End
         End
     End
 End
