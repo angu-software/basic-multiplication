f03993089374d66a05d6d4ca3ad747217f010d26
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-18 21:16:42 +0200

    [CD] Move next_rc_tag_from_tag to its own file and name the related spec file accordingly

---
+++ CDPipeline/lib/tags/next_rc_tag_from_tag.sh
@@ -0,0 +1,5 @@
+next_rc_tag_from_tag() {
+    local TAG=$1
+
+    echo $TAG
+}
--- CDPipeline/spec/lib/tags/next_rc_tag_from_tag_spec.sh
+++ CDPipeline/spec/lib/tags/next_rc_tag_from_tag_spec.sh
@@ -1,10 +1,6 @@
-next_rc_tag_from_tag() {
-    local TAG=$1
-
-    echo $TAG
-}
-
 Describe 'next_rc_tag_from_tag()'
+    Include 'lib/tags/next_rc_tag_from_tag.sh'
+    
     local TAG=''
 
     Describe 'when tag is empty'
