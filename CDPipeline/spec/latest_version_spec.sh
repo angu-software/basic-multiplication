3fe7c239af05948425a91b421ebfc12823cbb502
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-18 18:22:56 +0200

    [CD] extract split_tag function to its own file

--- CDPipeline/spec/latest_version_spec.sh
+++ CDPipeline/spec/latest_version_spec.sh
@@ -64,32 +64,6 @@ next_rc_tag_from_tag() {
     echo $TAG
 }
 
-Describe 'split_tag()'
-    TAG='1.0.0+1-RC'
-
-    It 'it splits the tag into its compnents'
-        When call split_tag $TAG
-        The output should equal '1.0.0 1 RC'
-    End
-
-    Describe 'when tag dose not contain RC indicator'
-        TAG='1.0.0+1'
-
-        It 'it splits the tag into its compnents'
-            When call split_tag $TAG
-            The output should equal '1.0.0 1'
-        End
-    End
-
-    Describe 'when tag is empty'
-        TAG=''
-        It 'it returns an empty string'
-            When call split_tag $TAG
-            The output should equal ''
-        End
-    End
-End
-
 Describe 'build_number_from_tag()'
     TAG='1.0.0+2-RC'
 
