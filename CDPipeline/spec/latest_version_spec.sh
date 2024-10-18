75d76fae4db463c5534205c9242b694939df53ef
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-18 14:42:28 +0200

    [CD] change version tag format and add function to extract build number from tag

--- CDPipeline/spec/latest_version_spec.sh
+++ CDPipeline/spec/latest_version_spec.sh
@@ -1,8 +1,28 @@
 
+build_number_from_tag() {
+    local TAG=$1
+
+    echo 1
+}
+
 make_new_rc_tag() {
-    TAG="1.0.0.1-RC"
+    local TAG="1.0.0+1-RC"
     echo $TAG
 }
+
+# Tag format: <major>.<minor>.<patch>+<build>-RC
+
+Describe 'build_number_from_tag()'
+    TAG='1.0.0+1-RC'
+
+    Describe 'when tag is empty'
+        TAG=''
+        It 'it returns 1'
+            When call build_number_from_tag ''
+            The output should equal 1
+        End
+    End
+End
 
 Describe 'make_new_rc_tag()'
     local TAG_LIST=''
@@ -12,18 +32,18 @@ Describe 'make_new_rc_tag()'
 
         It 'returns the first RC tag'
             When call make_new_rc_tag $TAG_LIST
-            The output should equal '1.0.0.1-RC'
+            The output should equal '1.0.0+1-RC'
         End
     End
 
     Describe 'when one tag exists'
         Pending 'Internal implementaion needs more refinement'
 
-        TAG_LIST='1.0.0.1-RC'
+        TAG_LIST='1.0.0+1-RC'
 
         It 'returns the next RC tag'
             When call make_new_rc_tag $TAG_LIST
-            The output should equal '1.0.0.2-RC'
+            The output should equal '1.0.0+2-RC'
         End
     End
 End
