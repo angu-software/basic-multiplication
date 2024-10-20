7f24f87128acc464ed03c019a4c0b08754aba63e
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-20 15:27:00 +0200

    [CD] Add function to set an rc version tag

--- CDPipeline/lib/git_repo/git_repo.sh
+++ CDPipeline/lib/git_repo/git_repo.sh
@@ -6,3 +6,8 @@ git_command() {
 get_rc_version_tags() {
     echo "$(git_command tag -l --sort=-v:refname)"
 }
+
+set_rc_version_tag() {
+    local TAG="$1"
+    git_command tag "$TAG"
+}
--- CDPipeline/spec/lib/git_repo/git_repo_spec.sh
+++ CDPipeline/spec/lib/git_repo/git_repo_spec.sh
@@ -1,23 +1,47 @@
 Describe 'git_repo.sh'
     Include 'lib/git_repo/git_repo.sh'
 
-    Mock git_command
-        case "$@" in
-            "tag -l --sort=-v:refname")
+    local NEW_RC_TAG="1.0.0+4-RC"
+    local EXISTING_RC_TAG="1.0.0+3-RC"
+
+    # Mocked git command
+    git_command() {
+        case "$1 $2" in
+            "tag -l")
                 echo "1.0.0+3-RC"
                 echo "1.0.0+2-RC"
                 echo "1.0.0+1-RC"
+                ;;
+            "tag 1.0.0+4-RC")
+                return 0
+                ;;
+            "tag $EXISTING_RC_TAG")
+                return 1
                 ;;
             *)
                 return 1
                 ;;
         esac
-    End
+    }
 
     Describe 'get_rc_version_tags()'
         It 'it returns a list of RC version tags'
             When call get_rc_version_tags
             The output should equal $'1.0.0+3-RC\n1.0.0+2-RC\n1.0.0+1-RC'
+        End
+    End
+
+    Describe 'set_rc_version_tag()'
+        It 'it sets the RC version tag'
+            When call set_rc_version_tag "$NEW_RC_TAG"
+            The status should be success
+        End
+
+        Describe 'when tag already exists'
+            It 'it does not set the RC version tag'
+                When call set_rc_version_tag "$EXISTING_RC_TAG"
+                The status should be failure
+            End
         End
     End
 End
