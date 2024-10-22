0b38cd09d6295c040cf5fd383bc5509e0640bd9c
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-22 10:09:20 +0200

    [JABOS] Implement tagging new RC in JABOS.sh

--- CDPipeline/JABOS.sh
+++ CDPipeline/JABOS.sh
@@ -1,1 +1,15 @@
 source 'jabos_paths.sh'
+
+source "$JABOS_LIB_DIR/git_repo/git_repo.sh"
+source "$JABOS_LIB_DIR/version_tag/version_tag.sh"
+
+# shellspec interception point
+# to sub/mock the used functions
+test || __() { :; }
+__ begin __
+
+fetch_tags
+rc_versions="$(get_rc_version_tags)"
+next_rc_tag="$(next_rc_tag_from_tag_list "$rc_versions")"
+set_rc_version_tag "$next_rc_tag" "$(git_sha)"
+push_tag "$next_rc_tag"
---
+++ CDPipeline/spec/JABOS_spec.sh
@@ -0,0 +1,39 @@
+Describe 'JABOS.sh'
+    SHA="1234567890"
+
+    Intercept begin 
+    __begin__() {
+        fetch_tags() {
+            echo 'fetch_tags'
+        }
+        get_rc_version_tags() {
+            echo "1.0.0+3-RC"
+        }
+        git_sha() {
+            echo "$SHA"
+        }
+        set_tag() {
+            echo "set_tag $@"
+        }
+        push_tag() {
+            echo "push_tag $@"
+        }
+    }
+
+    Describe 'when executed to tag a release candidate'
+        It 'fetches the git tags'
+            When run source ./JABOS.sh
+            The output should include 'fetch_tags'
+        End
+
+        It 'it sets the new rc tag'
+            When run source ./JABOS.sh
+            The output should include "set_tag 1.0.0+4-RC $SHA"
+        End
+
+        It 'it pushes the RC tag'
+            When run source ./JABOS.sh
+            The output should include "push_tag 1.0.0+4-RC"
+        End
+    End
+End
