3463044c8b65284a5ebecd02c187938296c1eefe
Andreas Günther <andreas.guenther@runtastic.com>
2024-10-23 15:20:54 +0200

    [JABOS] [Tidying] Move most relevant function in verify_version_tag to top of the file

--- CDPipeline/lib/version_tag/verify_version_tag.sh
+++ CDPipeline/lib/version_tag/verify_version_tag.sh
@@ -1,28 +1,5 @@
 source "$JABOS_FOUNDATION_DIR/foundation.sh"
 
-# https://stackoverflow.com/a/61835747 
-is_uint() {
-    case "$1" in 
-        *[0-9]*)
-            return 0
-            ;;
-        *)
-            return 1
-            ;;
-    esac
-}
-
-is_version() {
-    local version="$1"
-    local version_regex="^([0-9]+\.){2}[0-9]+$"
-    
-    if [[ "$version" =~ $version_regex ]]; then
-        return 0
-    fi
-
-    return 1
-}
-
 verify_rc_version_tag() {
     local tag="$1"
 
@@ -45,4 +22,27 @@ is_rc_version_tag() {
     if ! is_version "$tag_version" || ! is_uint "$tag_build" || [[ "$tag_rc" != "$RC_IDENTIFIER" ]]; then
         return 1
     fi
+}
+
+# https://stackoverflow.com/a/61835747 
+is_uint() {
+    case "$1" in 
+        *[0-9]*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+is_version() {
+    local version="$1"
+    local version_regex="^([0-9]+\.){2}[0-9]+$"
+    
+    if [[ "$version" =~ $version_regex ]]; then
+        return 0
+    fi
+
+    return 1
 }
