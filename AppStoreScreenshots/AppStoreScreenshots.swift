8fa963a193ee5f4fb1bf2fd347043b5565b38b5b
Andreas Günther <andreas.guenther@runtastic.com>
2024-11-15 23:07:58 +0100

    Add fastlane lane and fist draft app store snapshot test method

--- AppStoreScreenshots/AppStoreScreenshots.swift
+++ AppStoreScreenshots/AppStoreScreenshots.swift
@@ -7,37 +7,35 @@
 
 import XCTest
 
+@MainActor
 final class AppStoreScreenshots: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private lazy var robot = MultiplicationExerciseRobot()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        setupSnapshot(robot.app)
+        robot.launchApp()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        robot.terminateApp()
+
+        try super.tearDownWithError()
     }
 
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_snapshot_no_selection() throws {
+        snapshot("test")
     }
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func test_snapshot_correct_selection() throws {
+//
+//    }
+//
+//    func test_snapshot_wrong_selection() throws {
+//
+//    }
 }
