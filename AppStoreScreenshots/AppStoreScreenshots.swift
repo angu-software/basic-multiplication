c0a7f6bdc033ad180dae51a55dd5f77a50b4a825
Andreas Günther <andreas.guenther@runtastic.com>
2024-11-16 00:03:16 +0100

    Create new snapshots for App Store for all supported languages US, DE

--- AppStoreScreenshots/AppStoreScreenshots.swift
+++ AppStoreScreenshots/AppStoreScreenshots.swift
@@ -7,18 +7,22 @@
 
 import XCTest
 
+// TODO: extract the DSL into its own package and reference in UITests
+
 @MainActor
 final class AppStoreScreenshots: XCTestCase {
 
     private lazy var robot = MultiplicationExerciseRobot()
+    private var exercise: MultiplicationExerciseDSL!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
 
         continueAfterFailure = false
 
+        exercise = MultiplicationExerciseDSL(protocolDriver: robot)
         setupSnapshot(robot.app)
-        robot.launchApp()
+        try exercise.showsRandomExerciseWithOfferedSolutions()
     }
 
     override func tearDownWithError() throws {
@@ -28,14 +32,17 @@ final class AppStoreScreenshots: XCTestCase {
     }
 
     func test_snapshot_no_selection() throws {
-        snapshot("test")
+        snapshot("0_first_exercise")
     }
 
-//    func test_snapshot_correct_selection() throws {
-//
-//    }
-//
-//    func test_snapshot_wrong_selection() throws {
-//
-//    }
+    func test_snapshot_correct_selection() throws {
+        try exercise.selectSolution(.correct)
+        snapshot("1_correct_answer")
+    }
+
+    func test_snapshot_wrong_selection() throws {
+        try exercise.selectSolution(.wrong)
+
+        snapshot("2_wrong_answer")
+    }
 }
