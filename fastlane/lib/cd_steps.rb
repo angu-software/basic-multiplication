515033d5c90b064693a0a8579e9c55bb00fc744e
Andreas Günther <andreas.guenther@runtastic.com>
2024-11-17 16:30:49 +0100

    Extract method to capture app store screenshots from Fastfile to keep is responsibilities clear

--- fastlane/lib/cd_steps.rb
+++ fastlane/lib/cd_steps.rb
@@ -3,3 +3,4 @@
 require_relative 'steps/set_xcode_version'
 require_relative 'steps/build_for_testing'
 require_relative 'steps/test_without_building'
+require_relative 'steps/capture_store_screenshots'
---
+++ fastlane/lib/steps/capture_store_screenshots.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'build_for_testing'
+
+def capture_store_screenshots(devices:,
+                              languages:,
+                              scheme:,
+                              test_plan:,
+                              output_directory:,
+                              clear_previous_screenshots:)
+  stage = 'store_snapshot'
+
+  build_for_testing(stage: stage,
+                    scheme: scheme)
+
+  testing_build_paths = CDArtifactPaths.new(stage, 'store_snapshot_result')
+
+  capture_screenshots(devices: devices,
+                      languages: languages,
+                      scheme: scheme,
+                      testplan: test_plan,
+                      test_without_building: true,
+                      clear_previous_screenshots: clear_previous_screenshots,
+                      override_status_bar: true,
+                      override_status_bar_arguments: status_bar_settings,
+                      disable_package_automatic_updates: true,
+                      skip_package_dependencies_resolution: true,
+                      xcargs: '-quiet',
+                      output_directory: output_directory,
+                      derived_data_path: testing_build_paths.derived_data_path,
+                      cloned_source_packages_path: testing_build_paths.cloned_source_packages_path,
+                      buildlog_path: testing_build_paths.buildlog_path,
+                      result_bundle: true,
+                      namespace_log_files: true,
+                      headless: true,
+                      concurrent_simulators: false,
+                      stop_after_first_error: true,
+                      skip_open_summary: true)
+end
+
+def status_bar_settings
+  [
+    '--dataNetwork wifi',
+    '--wifiMode active',
+    '--wifiBars 3',
+    '--cellularMode active',
+    "--operatorName ''",
+    '--cellularBars 4',
+    '--batteryState charged',
+    '--batteryLevel 100',
+    '--time 2007-01-09T09:41:00.000+01:00Z' # The time set by fastlane by default is not a value IOS date/time string. it is missing the '.000' part after the seconds
+  ].join(' ')
+end
