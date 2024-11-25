648aa8b8ca6ee12d6e26205e34d221dfd6e42b37
Andreas Günther <andreas.guenther@adidas.com>
2024-11-25 13:29:26 +0100

    Play sounds for TCR success and TCR failure

--- TCR/tcr.sh
+++ TCR/tcr.sh
@@ -6,8 +6,10 @@ if ./scripts/buildIt.sh; then
     if ./scripts/test.sh; then
         echo "[TCR] Committing changes"
         ./scripts/commit.sh
+        ./scripts/play_sound.sh "${TCR_SUCCESS_SOUND}"
     else
         echo "[TCR] Reverting changes"
         ./scripts/revert.sh
+        ./scripts/play_sound.sh "${TCR_FAILURE_SOUND}"
     fi
 fi
