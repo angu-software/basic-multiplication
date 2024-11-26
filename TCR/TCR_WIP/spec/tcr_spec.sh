62c098633d32c95b6e7eb34a75948e32ab752edf
Andreas Günther <andreas.guenther@adidas.com>
2024-11-26 16:38:47 +0100

    Move path_spec to its own file

--- TCR/TCR_WIP/spec/tcr_spec.sh
+++ TCR/TCR_WIP/spec/tcr_spec.sh
@@ -4,17 +4,6 @@
 
 source './lib/path.sh'
 
-# -- path_spec.sh --
-
-Describe 'path'
-    Describe 'path_join'
-        It 'should join the paths with a delimiter'
-            When call path_join '/path/to' 'some' 'file'
-            The output should eq '/path/to/some/file'
-        End
-    End
-End
-
 # -- print.sh --
 
 source './lib/print.sh'
