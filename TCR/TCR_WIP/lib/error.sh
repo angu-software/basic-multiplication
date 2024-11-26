9db5331bdc48dbf464b08ed3a8bb736b919b2307
Andreas Günther <andreas.guenther@adidas.com>
2024-11-26 16:07:44 +0100

    Move error into its own file

---
+++ TCR/TCR_WIP/lib/error.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TCR_EXIT='exit'
+export TCR_EXIT
+
+error_message() {
+    local error="$1"
+
+    IFS='|' read -r _ message <<< "$error"
+    echo "$message"
+}
+
+error_code() {
+    local error="$1"
+
+    IFS='|' read -r code _ <<< "$error"
+    echo "$code"
+}
+
+raise_error() {
+    local error="$1"
+
+    print "$(error_message "$error")" "$TCR_OUTPUT_STDERR"
+    $TCR_EXIT "$(error_code "$error")"
+}
--- TCR/TCR_WIP/spec/tcr_spec.sh
+++ TCR/TCR_WIP/spec/tcr_spec.sh
@@ -71,28 +71,7 @@ TCR_ERROR_TCR_ALREADY_ENABLED='1|[TCR] TCR is already enabled'
 
 # -- error.sh --
 
-TCR_EXIT='exit'
-
-error_message() {
-    local error="$1"
-
-    IFS='|' read -r _ message <<< "$error"
-    echo "$message"
-}
-
-error_code() {
-    local error="$1"
-
-    IFS='|' read -r code _ <<< "$error"
-    echo "$code"
-}
-
-raise_error() {
-    local error="$1"
-
-    print "$(error_message "$error")" "$TCR_OUTPUT_STDERR"
-    $TCR_EXIT "$(error_code "$error")"
-}
+source './lib/error.sh'
 
 # -- exit_mock.sh --
 
