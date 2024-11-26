60698ce86a9155dd266182adbc2695d17bf86e36
Andreas Günther <andreas.guenther@adidas.com>
2024-11-26 20:11:16 +0100

    Extract lock_file_mock

--- TCR/TCR_WIP/spec/tcr_spec.sh
+++ TCR/TCR_WIP/spec/tcr_spec.sh
@@ -2,15 +2,22 @@
 
 source './spec/test_consts.sh'
 
+source './tcr'
+TCR_WORK_DIRECTORY='/current/work/directory'
+
+source './spec/test_doubles/exit_mock.sh'
+source './spec/test_doubles/lock_file_mock.sh'
+
 Describe 'tcr'
-    source './tcr'
 
     setup() {
         export TCR_OUTPUT_SILENT='true'
+        setup_exit_mock
+        setup_lock_file_mock
     }
     teardown() {
-        unset expected_lock_file_path
-        unset deleted_file_command
+        setup_exit_mock
+        setup_lock_file_mock
         unset TCR_OUTPUT_SILENT
         unset TCR_TEST_EXIT_STATUS
     }
@@ -18,30 +25,14 @@ Describe 'tcr'
     AfterEach 'teardown'
 
 # -- Mocks --
-    source './spec/test_doubles/exit_mock.sh'
 
-    TCR_WORK_DIRECTORY='/current/work/directory'
-
-    is_lock_file_existing() {
-        [ -n "$expected_lock_file_path" ]
-    }
-
-    touch() {
-        expected_lock_file_path="$1"
-    }
-
-    rm() {
-        deleted_file_command="_rm_ $1 $2"
-    }
-
-    setup_exit_mock
 
 # -- Examples --
 
     Describe 'when enabling tcr mode'
         It 'should creates a tcr lock file at the current work directory'
             When call tcr 'enable'
-            The variable expected_lock_file_path should eq '/current/work/directory/.tcr.lock'
+            The variable TRC_TEST_LOCK_FILE_PATH should eq '/current/work/directory/.tcr.lock'
         End
         
         It 'should inform that tcr mode is enabled'
@@ -66,7 +57,7 @@ Describe 'tcr'
     Describe 'when disabling tcr mode'
         It 'should delete the tcr lock file'
             When call tcr 'disable'
-            The variable deleted_file_command should eq '_rm_ -f /current/work/directory/.tcr.lock'
+            The variable TRC_TEST_LOCK_FILE_DELETE_CMD should eq '_rm_ -f /current/work/directory/.tcr.lock'
         End
 
         It 'should inform that tcr mode is disabled'
--- TCR/TCR_WIP/spec/test_doubles/exit_mock.sh
+++ TCR/TCR_WIP/spec/test_doubles/exit_mock.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+TCR_EXIT='exit_mock'
+
+exit_mock() {
+    TCR_TEST_EXIT_STATUS="$1"
+}
+
 setup_exit_mock() {
-    TCR_EXIT='exit_mock'
-    exit_mock() {
-        TCR_TEST_EXIT_STATUS="$1"
-    }
+    teardown_exit_mock
+}
+
+teardown_exit_mock() {
+    unset TCR_TEST_EXIT_STATUS
 }
