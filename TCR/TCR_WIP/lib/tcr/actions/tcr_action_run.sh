c3d87ebad2fac3ee51410962bd702eee24cfe117
Andreas Günther <andreas.guenther@adidas.com>
2024-11-28 10:57:55 +0100

    Tidy tcr_action_run

--- TCR/TCR_WIP/lib/tcr/actions/tcr_action_run.sh
+++ TCR/TCR_WIP/lib/tcr/actions/tcr_action_run.sh
@@ -4,65 +4,82 @@ TCR_ACTION_RUN='run'
 
 tcr_action_run() {
 
-    subject='Building'
-    execute_build_command
-    error_code=$?
+    execution_phase='Building'
+    execute_phase "$execution_phase"
+    phase_error_code=$?
 
-    if [ $error_code -eq 0 ]; then
-        subject='Testing'
-        execute_test_command
-        error_code_testing=$?
+    if is_success "$phase_error_code"; then
+        execution_phase='Testing'
+        execute_phase "$execution_phase"
+        phase_error_code=$?
 
-        if [ "$error_code_testing" -ne 0 ]; then
-            execute_revert_command
-            error_code_revert=$?
-            if [ "$error_code_revert" -ne 0 ]; then
-                subject='Reverting'
-                error_code="$error_code_revert"
-            else
-                error_code="$error_code_testing"
-            fi
+        if is_success "$phase_error_code"; then
+            execution_phase='Committing'
+            execute_phase "$execution_phase"
+            phase_error_code=$?
         else
-            subject='Committing'
-            execute_commit_command
-            error_code=$?
+            error_code_testing="$phase_error_code"
+
+            execution_phase='Reverting'
+            execute_phase "$execution_phase"
+            phase_error_code=$?
+
+            if is_success "$phase_error_code"; then
+                execution_phase='Testing'
+                phase_error_code="$error_code_testing"
+            fi
         fi
     fi
 
-    handle_error "$error_code" "$subject"
+    handle_error "$phase_error_code" "$execution_phase"
 }
 
 handle_error() {
     error_code="$1"
-    subject="$2"
+    execution_phase="$2"
 
-    if [ "$error_code" -ne 0 ]; then
-        error_raise "$(make_run_command_error "$error_code" "$subject")"
+    if ! is_success "$error_code"; then
+        error_raise "$(make_run_command_error "$error_code" "$execution_phase")"
     fi
 }
 
-execute_build_command() {
-    run_command "$TCR_RUN_BUILD_COMMAND"
-}
-
-execute_test_command() {
-    run_command "$TCR_RUN_TEST_COMMAND"
-}
-
-execute_commit_command() {
-    print_status 'Committing changes'
-    run_command "$TCR_RUN_COMMIT_COMMAND"
-}
-
-execute_revert_command() {
-    print_status 'Reverting changes'
-    run_command "$TCR_RUN_REVERT_COMMAND"
+execute_phase() {
+    phase="$1"
+    
+    case "$phase" in
+        'Building')
+            execute_phase_command '' "$TCR_RUN_BUILD_COMMAND"
+            ;;
+        'Testing')
+            execute_phase_command '' "$TCR_RUN_TEST_COMMAND"
+            ;;
+        'Reverting')
+            execute_phase_command "$phase" "$TCR_RUN_REVERT_COMMAND"
+            ;;
+        'Committing')
+            execute_phase_command "$phase" "$TCR_RUN_COMMIT_COMMAND"
+            ;;
+    esac
 }
 
 make_run_command_error() {
     error_code="$1"
-    subject="$2"
-    error_build "$error_code" "$subject failed with status $error_code"
+    execution_phase="$2"
+    error_build "$error_code" "$execution_phase failed with status $error_code"
+}
+
+execute_phase_command() {
+    phase="$1"
+    command="$2"
+
+    if [ -n "$phase" ]; then
+        print_status "$phase changes"
+    fi
+    run_command "$command"
+}
+
+is_success() {
+    [ "$1" -eq 0 ]
 }
 
 run_command() {
