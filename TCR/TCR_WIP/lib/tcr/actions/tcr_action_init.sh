7536e397245f55533e842177d1ddc2700bfb9bde
Andreas Günther <andreas.guenther@adidas.com>
2024-11-28 22:09:04 +0100

    Raise an error if a cfg file already exists in working dir when calling tcr init

--- TCR/TCR_WIP/lib/tcr/actions/tcr_action_init.sh
+++ TCR/TCR_WIP/lib/tcr/actions/tcr_action_init.sh
@@ -4,6 +4,8 @@ source "$TCR_HOME/lib/foundation.sh"
 
 TCR_ACTION_INIT='init'
 
+TCR_ERROR_TCR_INIT_CFG_EXISTS="$(error_build 10 'Config file already exists')"
+
 # Create template config
 # Do not create template cfg when it already exists
 # Template contains default reset and commit commands
@@ -11,7 +13,10 @@ TCR_ACTION_INIT='init'
 
 tcr_action_init() {
 
-    config_file_find
+    if config_file_exists_in_dir "$TCR_WORK_DIRECTORY"; then
+        error_raise "$TCR_ERROR_TCR_INIT_CFG_EXISTS"
+        return
+    fi
 
     config_file_create
 }
@@ -31,13 +36,23 @@ config_file_create() {
     file_create "$config_path"
 }
 
-config_file_find() {
-    file_find "$TCR_WORK_DIRECTORY" "*${TCR_CONFIG_FILE_EXTENSION}"
+config_file_exists_in_dir() {
+    local search_dir="$1"
+
+    files=$(config_file_find_in_dir "$search_dir")
+
+    is_set "$files"
+}
+
+config_file_find_in_dir() {
+    local search_dir="$1"
+
+    file_find "$1" "*${TCR_CONFIG_FILE_EXTENSION}"
 }
 
 file_find() {
     local search_dir="$1"
     local file_name_glob="$2"
 
-    find "$search_dir" -maxdepth 1 -type f -name "\"$file_name_glob\""
+    find "$search_dir" -maxdepth 1 -type f -name "$file_name_glob"
 }
--- TCR/TCR_WIP/spec/tcr_init_spec.sh
+++ TCR/TCR_WIP/spec/tcr_init_spec.sh
@@ -4,22 +4,21 @@ source './spec/test_doubles/work_dir_mock.sh'
 
 source './tcr'
 
+source './spec/test_doubles/exit_mock.sh'
 source './spec/test_doubles/file_mock.sh'
 
 Describe 'tcr init'
 
     setup() {
+        setup_exit_mock
         setup_file_mock
-    }
-    teardown() {
-        setup_file_mock
+        unset TEST_CFG_EXISTS
     }
     BeforeEach 'setup'
-    AfterEach 'teardown'
 
-    find() {
-        parameters="$*"
-        TEST_FILE_SEARCH_COMMAND="_find_ $parameters"
+    TEST_CFG_EXISTS=''
+    config_file_exists_in_dir() {
+        is_set "$TEST_CFG_EXISTS"
     }
 
     Context 'When executing tcr with init action'
@@ -28,18 +27,35 @@ Describe 'tcr init'
             The variable TCR_TEST_FILE_CREATE_PATH should eq '/current/work/directory/tcr.tcrcfg'
         End
 
-        It 'It searches for an existing config file'
-            When call tcr init
-            The variable TEST_FILE_SEARCH_COMMAND should eq '_find_ /current/work/directory -maxdepth 1 -type f -name "*.tcrcfg"'
-        End
-
         Context 'When config file already exists'
-            Pending
-
             It 'It raises an error'
+                TEST_CFG_EXISTS='true'
+            
                 When call tcr init
                 The error should eq '[TCR Error] Config file already exists'
+                The variable TCR_TEST_EXIT_STATUS should eq 10
             End
+        End
+    End
+End
+
+# -- config_file.sh -- #
+
+Describe 'config_file'
+
+    find() {
+        parameters="$*"
+
+        TEST_FILE_SEARCH_COMMAND="_find_ $parameters"
+
+        echo '/current/work/directory/tcr.tcrcfg'
+    }
+
+    Context 'When searhing for a file'
+        It 'It searches for file in a directory'
+            When call config_file_find_in_dir '/current/work/directory'
+            The variable TEST_FILE_SEARCH_COMMAND should eq '_find_ /current/work/directory -maxdepth 1 -type f -name *.tcrcfg'
+            The output should eq '/current/work/directory/tcr.tcrcfg'
         End
     End
 End
