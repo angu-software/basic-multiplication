c5533ece8ddd456477bcaf7d8c088f38ba1135be
Andreas Günther <andreas.guenther@adidas.com>
2024-11-29 08:59:03 +0100

    Restructure tcr run tests to give context when cfg file was found

--- TCR/TCR_WIP/spec/tcr_run_spec.sh
+++ TCR/TCR_WIP/spec/tcr_run_spec.sh
@@ -66,105 +66,108 @@ FILE_LIST
             The variable TCR_ACTION_RUN_CFG_PATH should eq "$TEST_CFG_FILE_PATH"
         End
 
-        # When a cfg file was found
-        # it reads its contained commands from the first found file
-        # it runs the build command ...
         # When no cfg file was found
         # It raises an error
 
-        It 'It runs the build command'
-            When call tcr run
-            The variable TCR_RUN_BUILD_EXECUTED_COMMAND should eq 'echo "Running build command"'
-        End
+        Context 'When a config file was found'
+        # When a cfg file was found
+        # it reads its contained commands from the first found file
+        # it runs the build command ...
 
-        It 'It tells that it builds the changes'
-            unset TCR_OUTPUT_SILENT
-
-            When call tcr run
-            The output should include '[TCR] Building changes'
-        End
-
-        Describe 'When the build command succeeds'
-            
-            It 'It runs the test command'
+            It 'It runs the build command'
                 When call tcr run
-                The variable TCR_RUN_TEST_EXECUTED_COMMAND should eq 'echo "Running test command"'
+                The variable TCR_RUN_BUILD_EXECUTED_COMMAND should eq 'echo "Running build command"'
             End
 
-            It 'It tells that it runs the tests'
+            It 'It tells that it builds the changes'
                 unset TCR_OUTPUT_SILENT
 
                 When call tcr run
-                The output should include '[TCR] Testing changes'
+                The output should include '[TCR] Building changes'
             End
 
-            Describe 'When the test command succeeds'
-                It 'It commits the changes'
+            Describe 'When the build command succeeds'
+                
+                It 'It runs the test command'
                     When call tcr run
-                    The variable TCR_RUN_COMMIT_EXECUTED_COMMAND should eq 'echo "Committing changes"'
+                    The variable TCR_RUN_TEST_EXECUTED_COMMAND should eq 'echo "Running test command"'
                 End
 
-                It 'It tells that it commits the changes'
+                It 'It tells that it runs the tests'
                     unset TCR_OUTPUT_SILENT
 
                     When call tcr run
-                    The output should include '[TCR] Committing changes'
-                    The error should not be present
+                    The output should include '[TCR] Testing changes'
                 End
 
-                Context 'When the commit command is failing'
-                    TCR_RUN_COMMIT_COMMAND_EXIT_STATUS=88
+                Describe 'When the test command succeeds'
+                    It 'It commits the changes'
+                        When call tcr run
+                        The variable TCR_RUN_COMMIT_EXECUTED_COMMAND should eq 'echo "Committing changes"'
+                    End
 
-                    It 'It raises an error'
+                    It 'It tells that it commits the changes'
                         unset TCR_OUTPUT_SILENT
 
                         When call tcr run
-                        The output should be present
-                        The error should eq '[TCR Error] Committing failed with status 88'
-                        The variable TCR_TEST_EXIT_STATUS should eq 88
+                        The output should include '[TCR] Committing changes'
+                        The error should not be present
+                    End
+
+                    Context 'When the commit command is failing'
+                        TCR_RUN_COMMIT_COMMAND_EXIT_STATUS=88
+
+                        It 'It raises an error'
+                            unset TCR_OUTPUT_SILENT
+
+                            When call tcr run
+                            The output should be present
+                            The error should eq '[TCR Error] Committing failed with status 88'
+                            The variable TCR_TEST_EXIT_STATUS should eq 88
+                        End
                     End
                 End
-            End
 
-            Describe 'When the test command fails'
-                TCR_RUN_TEST_COMMAND_EXIT_STATUS=66
-
-                It 'It raises an error'
-                    unset TCR_OUTPUT_SILENT
-
-                    When call tcr run
-                    The output should be present
-                    The error should eq '[TCR Error] Testing failed with status 66'
-                    The variable TCR_TEST_EXIT_STATUS should eq 66
-                End
-
-                It 'It reverts the changes'
-                    unset TCR_OUTPUT_SILENT
-
-                    When call tcr run
-                    The output should be present
-                    The error should be present
-                    The variable TCR_RUN_REVERT_EXECUTED_COMMAND should eq 'echo "Reverting changes"'
-                End
-
-                It 'It tells that it reverts the changes'
-                    unset TCR_OUTPUT_SILENT
-
-                    When call tcr run
-                    The output should include '[TCR] Reverting changes'
-                    The error should be present
-                End
-
-                Context 'When the revert command is failing'
-                    TCR_RUN_REVERT_COMMAND_EXIT_STATUS=77
+                Describe 'When the test command fails'
+                    TCR_RUN_TEST_COMMAND_EXIT_STATUS=66
 
                     It 'It raises an error'
                         unset TCR_OUTPUT_SILENT
 
                         When call tcr run
                         The output should be present
-                        The error should eq '[TCR Error] Reverting failed with status 77'
-                        The variable TCR_TEST_EXIT_STATUS should eq 77
+                        The error should eq '[TCR Error] Testing failed with status 66'
+                        The variable TCR_TEST_EXIT_STATUS should eq 66
+                    End
+
+                    It 'It reverts the changes'
+                        unset TCR_OUTPUT_SILENT
+
+                        When call tcr run
+                        The output should be present
+                        The error should be present
+                        The variable TCR_RUN_REVERT_EXECUTED_COMMAND should eq 'echo "Reverting changes"'
+                    End
+
+                    It 'It tells that it reverts the changes'
+                        unset TCR_OUTPUT_SILENT
+
+                        When call tcr run
+                        The output should include '[TCR] Reverting changes'
+                        The error should be present
+                    End
+
+                    Context 'When the revert command is failing'
+                        TCR_RUN_REVERT_COMMAND_EXIT_STATUS=77
+
+                        It 'It raises an error'
+                            unset TCR_OUTPUT_SILENT
+
+                            When call tcr run
+                            The output should be present
+                            The error should eq '[TCR Error] Reverting failed with status 77'
+                            The variable TCR_TEST_EXIT_STATUS should eq 77
+                        End
                     End
                 End
             End
